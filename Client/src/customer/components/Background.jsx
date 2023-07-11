09cda121d6cbd4ab6f62deb4f5f3372c9765461a
Shubham Bhagat <shubhambhagat575@gmail.com>
2023-07-11 23:28:50 +0530

    compressed carousel images

--- Client/src/customer/components/Background.jsx
+++ Client/src/customer/components/Background.jsx
@@ -3,7 +3,7 @@ import { MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from 'react-i
 
 const Background = ({ backgroundRef }) => {
 
-    const caraousel = ["car1.svg", "car2.svg", "car3.svg"];
+    const caraousel = ["Frame3.svg", "Frame22.svg", "Frame20.svg"];
     const [activeIndex, setActiveIndex] = useState(0);
     const caraouselRef = useRef([]);
 
@@ -24,10 +24,8 @@ const Background = ({ backgroundRef }) => {
             {caraousel.map((item, index) => {
                 return <div key={index} ref={e => caraouselRef.current[index] = e} className={`min-w-full h-full ease-in-out transition-transform duration-1000 bg-[url(/assets/svgs/${item})] bg-no-repeat bg-cover flex flex-col`}>
                     <aside id='headings' className='w-1/2 h-full relative flex flex-col items-start lg:px-20 md:px-14 sm:px-8 px-4 justify-center'>
-                        <h3 className='cursor-default lg:text-2xl md:text-xl sm:text-base text-base font-viga text-yellow-400'>Beats Solo</h3>
-                        <h2 className='cursor-default lg:text-7xl md:text-5xl sm:text-3xl text-xl font-viga text-green-400 '>Wireless</h2>
-                        <h3 className='cursor-default lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-viga text-white font-bold'>HEADPHONES</h3>
-                        <button className='lg:p-2 lg:px-4 lg:text-lg md:text-base md:p-1 md:px-3 p-[0.2rem] px-2 my-1 text-sm  transition-transform hover:scale-110 text-white bg-red-500 rounded-md'>Shop Now</button>
+                   
+             
                     </aside>
                 </div>
             })}
